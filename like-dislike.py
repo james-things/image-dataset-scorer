9dbbbf2c0500b9b4d49ac27c1c560ed73480cad0
James <jharr651@students.kennesaw.edu>
2023-07-26 18:23:43 -0400

    Add detailed comments, clean up formatting, remove unused var

--- like-dislike.py
+++ like-dislike.py
@@ -5,7 +5,7 @@ import json
 import shutil
 from PIL import Image, ImageTk
 
-# utility function to get image files, filtering already rated images
+# Utility function to get image files, filtering already rated images
 def find_images(directory, ratings):
     images = []
     for root, dirs, files in os.walk(directory):
@@ -16,7 +16,7 @@ def find_images(directory, ratings):
                     images.append(image_path)
     return images
 
-
+# Primary application class
 class ImageBrowser:
     def __init__(self):
         self.root = tk.Tk()
@@ -26,7 +26,6 @@ class ImageBrowser:
         # Create frames for ImagePanel and ButtonsPanel
         self.image_frame = tk.Frame(self.root, bg='white', bd=2, relief='groove')
         self.image_frame.pack(fill='both', expand=True, side='left', padx=(10,5), pady=10)
-        
         self.buttons_frame = tk.Frame(self.root, bg='white', bd=2, relief='groove')
         self.buttons_frame.pack(fill='both', expand=False, side='right', padx=(5,10), pady=10)
 
@@ -45,7 +44,7 @@ class ImageBrowser:
         self.directory_entry = tk.Entry(self.buttons_frame)
         self.directory_entry.pack(side='top', padx=5, pady=5, fill='x')
 
-        self.root.geometry('1600x900') 
+        self.root.geometry('1600x900') # currently hardcoded for a 2k display
 
         # Attributes
         self.images = []  # list of all image paths
@@ -54,13 +53,14 @@ class ImageBrowser:
         self.directory = ""
         self.ratings_file_path = ""
         self.prev_image_index = None 
-        self.last_rated_image = None 
         self.load_directory()
 
+    # Create a tkinter button with the appropriate parameters
     def create_button(self, frame, text, command, color):
         button = tk.Button(frame, text=text, command=command, font=("Arial", 10), bg=color, padx=20, pady=10, width=20)
         button.pack(side='top', padx=5, pady=5)
 
+    # Step user through process to load image data and define ratings file
     def load_directory(self):
         self.directory = filedialog.askdirectory()
         if not self.directory:
@@ -97,18 +97,20 @@ class ImageBrowser:
         self.current_image = -1 if self.images else None
         self.next_image()  # Display the first image
 
+    # Load a target ratings JSON
     def load_ratings(self, ratings_file):
         try:
             with open(ratings_file, 'r') as file:
                 self.ratings = json.load(file)
         except Exception:
-            print(f"Loading ratings failed! {Exception}")
-            pass  # It's okay if the file doesn't exist
+            pass  # discard any exception
 
+    # Save ratings to the defined ratings file
     def save_ratings(self, ratings_file):
         with open(ratings_file, 'w') as file:
             json.dump(self.ratings, file, indent=4)
 
+    # Resize an image to fit within the display area
     def resize_image(self, img_path):
         # Fetch the size of the image panel
         panel_width = self.image_frame.winfo_width()
@@ -129,13 +131,14 @@ class ImageBrowser:
 
         return self.photo_image
 
+    # Clear the image in the display area (used when we reach last image)
     def clear_image(self):
         # Create a blank image and display it in the label
         blank_image = ImageTk.PhotoImage(Image.new('RGB', (1, 1)))
         self.image_label.config(image=blank_image)
         self.image_label.image = blank_image  # Keep a reference to avoid garbage collection
 
-
+    # Advance to the next queued image
     def next_image(self):
         if self.current_image is not None:
             self.prev_image_index = self.current_image
@@ -150,6 +153,7 @@ class ImageBrowser:
             self.current_image = None  # No more images
             self.clear_image()  # Clear the image
 
+    # Step backwards to the previously viewed image
     def prev_image(self):
         # Move to the previous image
         if self.prev_image_index is not None and self.prev_image_index >= 0:
@@ -160,20 +164,24 @@ class ImageBrowser:
             photo_image = self.resize_image(path)
             self.image_label.config(image=photo_image)
 
+    # Rate the currently viewed image
     def rate_image(self, rating):
         if self.current_image is not None:
             path = self.images[self.current_image]
             self.ratings[path] = rating
-            self.save_ratings(self.ratings_file_path)  # Save after each rating in case the program crashes
+            self.save_ratings(self.ratings_file_path)  # Save after each rating in case the program terminates unexpectedly
 
+    # Apply a positive rating and advance to the next image
     def like(self):
         self.rate_image('like')
         self.next_image()
 
+    # Apply a negative rating and advance to the next image
     def dislike(self):
         self.rate_image('dislike')
         self.next_image()
 
+    # Copy all positively rated images and associated metadata to target directory
     def copy_liked(self):
         copy_to_dir = filedialog.askdirectory()
         if not copy_to_dir:
@@ -188,7 +196,7 @@ class ImageBrowser:
                 if os.path.exists(txt_path):
                     shutil.copy(txt_path, copy_to_dir)
 
-
+# Script entry point
 if __name__ == "__main__":
     app = ImageBrowser()
     app.root.mainloop()
